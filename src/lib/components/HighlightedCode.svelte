b7442c042e7efcf65f58539aa2863303fa4c5a33
mahmoud-zino <mahmoud.alhalaby@gmail.com>
2025-12-29 19:51:48 +0100

    implement get started section

---
+++ src/lib/components/HighlightedCode.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import { codeToHtml } from 'shiki';
+
+	interface Props {
+		code: string;
+		lang?: string;
+	}
+
+	let { code, lang = 'csharp' }: Props = $props();
+
+	let highlightedHtml = $state('');
+
+	$effect(() => {
+		const highlight = async () => {
+			highlightedHtml = await codeToHtml(code, {
+				lang,
+				theme: 'github-dark',
+				transformers: [
+					{
+						line(node) {
+							node.properties['data-line'] = '';
+						},
+						code(node) {
+							node.properties.class = 'line-numbers';
+						}
+					}
+				]
+			});
+		};
+		highlight();
+	});
+</script>
+
+{#if highlightedHtml}
+	<div class="code-container overflow-x-auto">
+		<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+		{@html highlightedHtml}
+	</div>
+{:else}
+	<div class="overflow-x-auto">
+		<pre class="rounded-lg bg-surface-900 p-6"><code class="text-xs text-surface-50">{code}</code
+			></pre>
+	</div>
+{/if}
+
+<style>
+	:global(.code-container pre) {
+		font-size: 0.75rem;
+		padding: 1.5rem;
+		border-radius: 0.5rem;
+	}
+
+	:global(.code-container code.line-numbers) {
+		counter-reset: line;
+	}
+
+	:global(.code-container code.line-numbers > [data-line]::before) {
+		counter-increment: line;
+		content: counter(line);
+		display: inline-block;
+		width: 2rem;
+		margin-right: 1.5rem;
+		text-align: right;
+		color: #6b7280;
+		user-select: none;
+	}
+</style>
